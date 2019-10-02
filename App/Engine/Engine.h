5fc8f20d5e5a1dfab582e96224d544e6ad10e127
8symbols <32729802+8symbols@users.noreply.github.com>
2019-10-03 06:51:34 +1000

    change engine's api

--- App/Engine/Engine.h
+++ App/Engine/Engine.h
@@ -1,13 +1,19 @@
 #ifndef OURSQL_APP_ENGINE_ENGINE_H_
 #define OURSQL_APP_ENGINE_ENGINE_H_
 
+#include <fstream>
+#include <sstream>
+#include <cstdio>
 #include "Table.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
-bool create(const Table& table){};
+bool create(const Table& table);
 
 Table show(const std::string& name);
 
-std::string showCreate();
+std::string showCreate(const std::string& name);
 
 bool drop(const std::string& name);
 
