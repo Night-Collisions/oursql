a12546179cb65157ecb42ec4bbc327c42869fe57
Victor <priorityvctr@gmail.com>
2019-10-06 13:53:35 +1000

    updata main

--- App/main.cpp
+++ App/main.cpp
@@ -5,7 +5,11 @@
 int main() {
 
     ParserManager pm;
-    auto a = pm.getParseTree(stdin);
+
+    //auto a = pm.getParseTree();  // read from stdin by default
+    auto a = pm.getParseTree("show create table a;"); //or give a query string
+
+    delete a; // это тоже работает
 
     return 0;
 }
