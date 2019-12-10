e659a2d32419519d1dc876e024fbb0abe03ce8ca
Danila Goriachkin <danila.goriachkin@tra.ai>
2019-12-10 17:29:44 +1000

    Added thms.

--- Tests/RequestGenerator.cpp
+++ Tests/RequestGenerator.cpp
@@ -1,33 +1,29 @@
 #include "RequestGenerator.h"
 
-#include <iostream>
 #include <fstream>
-
-unsigned int NodeRequestGenerator::getMass() const {
-    unsigned int children_mass = 0;
-    for (const auto &i : children) {
-        children_mass += i->getMass();
-    }
-    return std::max(1U, std::max(1U, names.size()) * children_mass);
-}
+#include <iostream>
 
 RequestGenerator::RequestGenerator(const std::string& file) {
     createTree(file);
 }
 
 RequestGenerator::~RequestGenerator() {
-    delete tree.head;
+    //    delete tree.head;
 }
 
 void addNodeChildrens(NodeRequestGenerator* node, std::ifstream& f);
 
-std::vector<std::string> name2names(const std::string &s) {
+std::vector<std::string> name2names(const std::string& s) {
     std::vector<std::string> ans(1);
-    for (auto &i: s) {
-        if (i == ' ' || i == '\n') {
+    for (auto& i : s) {
+        if (i == '\n') {
             continue;
         }
         if (i == '|') {
+            while (ans.back().size() > 0 &&
+                   (ans.back().back() == ' ' || ans.back().back() == '\n')) {
+                ans.back().pop_back();
+            }
             ans.emplace_back();
         } else {
             ans.back().push_back(i);
@@ -40,13 +36,14 @@ NodeRequestGenerator* getNode(std::ifstream& f) {
     char c;
     while (!(f >> c) && c != ' ') {
     }
-    if (c == '/' || c == EOF) {
+    if (c == '/') {
         return nullptr;
     }
     std::string name;
-    while (c != '\\' && c != EOF) {
-        name.push_back(c);
-        f >> c;
+    int c_int = c;
+    while (c_int != '\\' && c_int != EOF) {
+        name.push_back(c_int);
+        c_int = f.get();
     }
     NodeRequestGenerator* new_node = new NodeRequestGenerator(name2names(name));
     addNodeChildrens(new_node, f);
@@ -57,7 +54,7 @@ void addNodeChildrens(NodeRequestGenerator* node, std::ifstream& f) {
     NodeRequestGenerator* children;
     children = getNode(f);
     while (children != nullptr) {
-        node->children.push_back(children);
+        node->addChild(children);
         children = getNode(f);
     }
 }
@@ -69,8 +66,75 @@ void RequestGenerator::createTree(const std::string& file) {
         return;
     }
 
-    delete tree.head;
-    tree.head = getNode(f);
+    // TODO: delete
+
+    std::string name;
+    NodeRequestGenerator* node;
+    while (f >> name) {
+        node = getNode(f);
+        if (name.front() == '+' || name.front() == '-') {
+            trees_.insert({name, node});
+        } else {
+            std::cerr << "The file is damaged!" << std::endl;
+            return;
+        }
+    }
     f.close();
+
+    max_mass_ = 0;
+    auto it = trees_.begin();
+    while (it != trees_.end()) {
+        if (it->first.size() > 0 && it->first.front() == '+') {
+            max_mass_ += it->second->getMass();
+        }
+    }
 }
 
+std::string RequestGenerator::getRecuest() {
+    unsigned int numb = rand() % max_mass_;
+    unsigned int sum = 0;
+    NodeRequestGenerator *node;
+    auto it = trees_.begin();
+    while (it != trees_.end()) {
+        if (it->first.size() > 0 && it->first.front() == '+') {
+            sum += it->second->getMass();
+            if (sum >= numb) {
+                node = it->second;
+                break;
+            }
+        }
+    }
+    return createRequest(node) + ';';
+}
+
+std::string RequestGenerator::implCommand(std::string &s) {
+    if (s.size() > 0) {
+        if (s.front() == '~') {
+            auto it = comands_.find(s);
+            if (it == comands_.end()) {
+                std::cerr << s << ":: is wrong command!" << std::endl;
+                return "";
+            }
+            //TODO:: вырезать подстроку
+            return it->second();
+        } else if (s.front() == '-' || s.front() == '+') {
+            auto it = trees_.find(s);
+            if (it == trees_.end()) {
+                std::cerr << s << ":: is wrong command!" << std::endl;
+                return "";
+            }
+            return createRequest(it->second);
+        } else {
+            return s;
+        }
+    }
+    return "";
+}
+
+std::string RequestGenerator::createRequest(NodeRequestGenerator *node) {
+    std::string ans;
+    auto names = node->getNames();
+    std::string command = names[rand() % names.size()];
+    ans += " " + implCommand(command);
+    //TODO: пройтись по детям
+}
--- Tests/RequestGenerator.h
+++ Tests/RequestGenerator.h
@@ -1,35 +1,49 @@
 #ifndef OURSQL_REQUESTGENERATOR_H
 #define OURSQL_REQUESTGENERATOR_H
 
+#include <map>
 #include <string>
 #include <vector>
 
 class NodeRequestGenerator {
    public:
-    NodeRequestGenerator(const std::vector<std::string>& names_)
-        : names(names_) {}
+    NodeRequestGenerator(const std::vector<std::string>& names)
+        : names_(names) {}
 
     ~NodeRequestGenerator() {
-        for (auto& i : children) {
+        for (auto& i : children_) {
             delete i;
         }
     }
 
-    unsigned int getMass() const;
+    unsigned int getMass() const { return std::max(1U, mass_); }
 
-    std::vector<std::string> names;
-    std::vector<NodeRequestGenerator*> children;
-};
-
-class TreeRequest {
-   public:
-    ~TreeRequest() { delete head; }
-
-    unsigned int getMass() const {
-        return (head == nullptr) ? (0) : (head->getMass());
+    unsigned int getChildrenMass() const {
+        unsigned int ans = 0;
+        for (auto &i : children_) {
+            ans += i->getMass();
+        }
+        return ans;
     }
 
-    NodeRequestGenerator* head = nullptr;
+    std::vector<std::string> getNames() { return names_; }
+    void addName(const std::string& s) {
+        names_.push_back(s);
+        if (names_.size() > 1) {
+            mass_ = mass_ / (names_.size() - 1) * names_.size();
+        }
+    }
+
+    std::vector<NodeRequestGenerator*> getChildren() { return children_; }
+    void addChild(NodeRequestGenerator* c) {
+        children_.push_back(c);
+        mass_ += c->getMass() * names_.size();
+    }
+
+   protected:
+    std::vector<std::string> names_;
+    std::vector<NodeRequestGenerator*> children_;
+    unsigned int mass_ = 0;
 };
 
 class RequestGenerator {
@@ -37,10 +51,41 @@ class RequestGenerator {
     RequestGenerator(const std::string& file);
     ~RequestGenerator();
 
+    std::string getRecuest();
+
    private:
     void createTree(const std::string& file);
 
-    TreeRequest tree;
+    std::string createRequest(NodeRequestGenerator *node);
+    std::string implCommand(std::string &);
+
+    static std::string getChar(const std::string &s = "") {}
+    static std::string getWord(const std::string &s = "") {}
+    static std::string getIntNumber(const std::string &s = "") {}
+    static std::string getFloatNumber(const std::string &s = "") {}
+    static std::string getMathExpression(const std::string &s = "") {}
+    static std::string getBoolExpression(const std::string &s = "") {}
+    static std::string getArrayOf(const std::string &s) {}
+    static std::string getField(const std::string &s = "") {}
+    static std::string getTable(const std::string &s = "") {}
+    static std::string getMathExpressionWithField(const std::string &s = "") {}
+    static std::string getBoolExpressionWithField(const std::string &s = "") {}
+
+    std::map<std::string, NodeRequestGenerator*> trees_;
+    const std::map<std::string, std::string (*)(const std::string &)> comands_ = {
+        {"~char", getChar},
+        {"~word", getWord},
+        {"~int_number", getIntNumber},
+        {"~float_number", getFloatNumber},
+        {"~math_expr", getMathExpression},
+        {"~bool_expr", getBoolExpression},
+        {"~array_of", getArrayOf},
+        {"~field", getField},
+        {"~table", getTable},
+        {"~math_expr_with_field", getMathExpressionWithField},
+        {"~bool_expr_with_field", getBoolExpressionWithField}
+    };
+    unsigned int max_mass_ = 0;
 };
 
 #endif  // OURSQL_REQUESTGENERATOR_H
