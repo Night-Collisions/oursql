4a47a5c4f3a052007d45f47984f045b9bec3b6b3
8symbols <32729802+8symbols@users.noreply.github.com>
2019-12-20 23:04:47 +1000

    fix bug

--- Server/Engine/Engine.cpp
+++ Server/Engine/Engine.cpp
@@ -85,9 +85,11 @@ void Engine::insertIntoTransactionsEndTimesTable(int id){
     std::vector<Value> values;
     values.push_back({std::to_string(id)});
     values.push_back({std::to_string(posix_time)});
+    beginTransaction(0);
     Cursor cursor(0, kTransactionsEndTimesTable);
     cursor.insert(values);
     cursor.commit();
+    endTransaction(0);
 }
 
 
